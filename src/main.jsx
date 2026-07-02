7639067434c77cf85c879a756545be9027b51c08
Caleb Saldana <133138386+Xtqm@users.noreply.github.com>
2026-07-02 16:07:15 -0400

    minor fix for consistency in imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
